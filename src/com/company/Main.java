1e120098d555fc9e969f82c8f76f10d2256e5a8a
79993 <anatolijkostucenko6@gmail.com>
2021-10-16 19:12:48 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        massiveTwo();
+        MyArraySizeException();
+
+    }
+    public static void massiveTwo(){
+        int[][] arr = new int[4][4];
+try {
+
+} catch(MyArraySizeException arr){
+
+} catch(MyArrayDataException arr){
+
+}finally{
+System.out.println("Всё верно");
+}
+    }
+   public static void MyArraySizeException(){
+
+    }
+
+}
